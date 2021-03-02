44e768497072bf75e97457e88cb10e7018849e2b
jonl500 <jonl500@gmail.com>
2021-03-01 20:17:14 -0500

    Updated TCP

--- src/NetworkingTCP_Client.java
+++ src/NetworkingTCP_Client.java
@@ -1,37 +1,65 @@
 
 //This is the client
-
+import java.util.Random;
 import java.io.*;
 import java.net.*;
+
 public class NetworkingTCP_Client {
+    static final  int portNumber = 2800;
+
+    public static String createMsg(int input){
+        StringBuilder msg = new StringBuilder("");
+        for (int i = 0; i < input/2; i++) {
+           msg.append('a');
+        }
+
+       return msg.toString();
+    }
+    //int key = 0;
+    public static String xor(String message){
+        //XOR key
+        int key = 7;
+        // Define String to store encrypted/decrypted String
+        String outputStr = "";
+        int len = message.length();
+        // perform XOR operation of key
+        // with every character in string
+        for (int i = 0; i < len; i++){
+           Character.toString((char)(message.charAt(i) ^ key));
+
+        }
+
+        System.out.println(outputStr);
+        return outputStr;
+    }
 
         public static void main(String[] args) throws IOException {
 
-            if (args.length != 2) {
-                System.err.println(
-                        "Usage: java EchoClient <host name> <port number>");
-                System.exit(1);
-            }
+            String hostName = "rho.cs.oswego.edu";
 
-            String hostName = args[0];
-            int portNumber = Integer.parseInt(args[1]);
 
-            try (
-                    Socket echoSocket = new Socket(hostName, portNumber);
-                    PrintWriter out =
-                            new PrintWriter(echoSocket.getOutputStream(), true);
-                    BufferedReader in =
-                            new BufferedReader(
-                                    new InputStreamReader(echoSocket.getInputStream()));
-                    BufferedReader stdIn =
-                            new BufferedReader(
-                                    new InputStreamReader(System.in))
-            ) {
+            try (Socket echoSocket = new Socket(hostName, portNumber);
+                    PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
+                    BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+                    BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))) {
                 String userInput;
-                while ((userInput = stdIn.readLine()) != null) {
-                    out.println(userInput);
-                    System.out.println("echo: " + in.readLine());
+                long nanoStartTime = System.nanoTime();
+                boolean sent = false;
+                while (sent != true) {
+                    System.out.println("Specify the amount of bytes you'd like to send:");
+                    userInput = stdIn.readLine();
+                    //this is where I switch to bytes
+                    out.write(xor(createMsg(Integer.parseInt(userInput))));
+                    //out sends out
+                    System.out.println(in.readLine());
+                    sent = true;
+                    //in receives from server
                 }
+
+                long nanoEndTime = System.nanoTime();
+                long RTT = nanoEndTime - nanoStartTime;
+                echoSocket.close();
+                System.out.println(RTT);
             } catch (UnknownHostException e) {
                 System.err.println("Don't know about host " + hostName);
                 System.exit(1);
---
+++ src/ServerSide_TCP.java
@@ -0,0 +1,36 @@
+
+import java.net.*;
+import java.io.*;
+
+public class ServerSide_TCP {
+    static final int portNumber = 2800;
+    public static void main(String[] args) throws IOException {
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(portNumber);
+            for (;;) {
+                Socket client = serverSocket.accept();
+
+                PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+                String cmd = in.readLine();
+
+                String reply = cmd;
+                int len = reply.length();
+
+
+                out.println("Content-Length: " + len);
+                out.println(reply);
+
+                out.close();
+                in.close();
+                client.close();
+            }
+        } catch (IOException e) {
+            System.out.println("Exception caught when trying to listen on port "
+                    + portNumber + " or listening for a connection");
+            System.out.println(e.getMessage());
+        }
+    }
+}
