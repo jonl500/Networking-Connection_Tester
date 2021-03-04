d929075b6dda0e7a4ab5945066cc58998641e1a7
jonl500 <jonl500@gmail.com>
2021-03-03 19:43:18 -0500

    TCP server XOR attempt

--- src/ServerSide_TCP.java
+++ src/ServerSide_TCP.java
@@ -4,6 +4,29 @@ import java.io.*;
 
 public class ServerSide_TCP {
     static final int portNumber = 2800;
+
+    public static String xor(String message){
+        //XOR key
+        String key = "abcdefh";
+        StringBuilder blid = new StringBuilder();
+        // Define String to store encrypted/decrypted String
+        String outputStr = "";
+        int len = message.length();
+        // perform XOR operation of key
+        // with every character in string
+        int j =0;
+        for (int i = 0; i < len; i++){
+            blid.append((char)(message.charAt(i) ^ key.charAt(i%key.length())));
+            j++;
+            if(j%8 == 0){
+                j = 0;
+            }
+
+        }
+
+        System.out.println(blid.toString());
+        return blid.toString();
+    }
     public static void main(String[] args) throws IOException {
         System.out.println("Server is open");
         try {
@@ -11,14 +34,12 @@ public class ServerSide_TCP {
             System.out.println("port 2800 established");
             for (;;) {
                 Socket client = serverSocket.accept();
-
-                PrintWriter out = new PrintWriter(client.getOutputStream(), true);
                 BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-
+                PrintWriter out = new PrintWriter(client.getOutputStream(), true);
                 System.out.println("listening for input");
 
                 String cmd;
-                while ((cmd = in.readLine()) != null) {
+                while ((cmd = xor(in.readLine())) != null) {
                     System.out.println("reading line");
                     //String reply = cmd;
                     int len = cmd.length();
@@ -26,7 +47,7 @@ public class ServerSide_TCP {
 
                     out.println("Content-Length: " + len);
 
-                    out.println(cmd);
+                    out.println(xor(cmd));
                 }
                     out.close();
                     in.close();
